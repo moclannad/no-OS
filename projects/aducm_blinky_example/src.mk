5c2fd216ea6e4b2e4290f2fce1661deadb0ab180
Mihail Chindris <mihail.chindris@analog.com>
2020-05-21 19:06:31 +0300

    project: aducm_blinky_example: Add aducm sample project
    This projects uses the new added makefile for aducm.
    The functionality is just to blink the leds on the evaluation board
    with a specific pattern.
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

---
+++ projects/aducm_blinky_example/src.mk
@@ -0,0 +1,39 @@
+################################################################################
+#									       #
+#     Shared variables:							       #
+#	- PROJECT							       #
+#	- DRIVERS							       #
+#	- INCLUDE							       #
+#	- PLATFORM_DRIVERS						       #
+#	- NO-OS								       #
+#									       #
+################################################################################
+
+#In aducm projects the name of the source dir should be different from src
+#Direcory where app srcs are stored
+
+SRC_DIRS +=	$(PROJECT)/app_src		\
+		$(PLATFORM_DRIVERS)		\
+		$(INCLUDE)			\
+		$(NO-OS)/util			\
+		$(DRIVERS)/accel/adxl345	\
+		$(DRIVERS)/sd-card	 	\
+		$(NO-OS)/libraries/fatfs
+
+
+
+#DFP
+#SRC_DIRS += $(NO-OS)/cces/srcs
+
+#Include makefiles from each source directory if they exist
+SUB_MAKES= $(wildcard $(addsuffix /src.mk, $(SRC_DIRS)))
+include $(SUB_MAKES)
+
+DIRECORIES_WITH_MAKEFILES = $(patsubst %/src.mk, %,$(SUB_MAKES))
+REMAINING_DIRECORIES = $(filter-out $(DIRECORIES_WITH_MAKEFILES), $(SRC_DIRS))
+
+# recursive wildcard
+rwildcard = $(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+
+SRCS += $(foreach dir, $(REMAINING_DIRECORIES), $(call rwildcard, $(dir),*.c))
+INCS += $(foreach dir, $(REMAINING_DIRECORIES), $(call rwildcard, $(dir),*.h))
